96ca5e9ec581189e282f632992ffccc04e772a2b
Chuhj <chj9574@naver.com>
2021-10-16 14:32:46 +0900

    Add fetching favorited in FavoriteButton

--- client/src/components/views/MovieDetail/Sections/FavoriteButton.js
+++ client/src/components/views/MovieDetail/Sections/FavoriteButton.js
@@ -9,29 +9,53 @@ function FavoriteButton({ userFrom, movieInfo }) {
   const movieRuntime = movieInfo.runtime;
 
   const [favoriteNumber, setFavoriteNumber] = useState(0);
+  const [favorited, setFavorited] = useState(false);
 
   // 저장할 정보: userFrom(userId), movieId, movieTitle, movieImage, movieRuntime
 
-  useEffect(async () => {
-    if (movieId) {
-      try {
-        // favorite 수를 불러옴
-        const data = { movieId };
-        const res = await axios.post('/api/favorite/favoriteNumber', data);
-        console.log(res.data);
-        if (res.data.success) {
-          setFavoriteNumber(res.data.favoriteNumber);
-        } else {
+  useEffect(() => {
+    const fetchData = async () => {
+      if (movieId) {
+        try {
+          // favorite 수를 불러옴
+          const data = { movieId };
+          const res = await axios.post('/api/favorite/favoriteNumber', data);
+
+          if (res.data.success) {
+            setFavoriteNumber(res.data.favoriteNumber);
+          } else {
+            alert('Failed to fetch favorite number!');
+          }
+        } catch (err) {
           alert('Failed to fetch favorite number!');
+          console.log(err);
         }
-      } catch (err) {
-        alert('Failed to fetch favorite number!');
-        console.log(err);
+
+        try {
+          // favorited 여부를 불러옴
+          const data = { movieId, userFrom };
+          const res = await axios.post('/api/favorite/favorited', data);
+          console.log(res.data);
+          if (res.data.success) {
+            setFavorited(res.data.favorited);
+          } else {
+            alert('Failed to fetch favorited!');
+          }
+        } catch (err) {
+          alert('Failed to fetch favorited!');
+          console.log(err);
+        }
       }
-    }
+    };
+    fetchData();
   }, [movieId]);
 
-  return <Button>Favorite</Button>;
+  return (
+    <Button>
+      {favorited ? 'Remove from favorites ' : 'Add to favorites '}
+      {favoriteNumber}
+    </Button>
+  );
 }
 
 export default FavoriteButton;
