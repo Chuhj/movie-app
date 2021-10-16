e39cda4ead498f0cf5a757210f367d3714fef64f
Chuhj <chj9574@naver.com>
2021-10-16 14:30:57 +0900

    Add favorited method to Favorite router

--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -12,4 +12,17 @@ router.post('/favoriteNumber', (req, res) => {
   });
 });
 
+router.post('/favorited', (req, res) => {
+  Favorite.find(
+    { movieId: req.body.movieId, userFrom: req.body.userFrom },
+    (err, favorites) => {
+      // Favorite 모델에서 movieId와 userFrom 일치하는 데이터를 찾아 있다면 true 없다면 false 반환
+      if (err) res.status(400).send(err);
+
+      const favorited = favorites ? false : true;
+      res.json({ success: true, favorited });
+    }
+  );
+});
+
 module.exports = router;
